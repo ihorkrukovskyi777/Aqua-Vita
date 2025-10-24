5c2bbacfe8d53f0f354dacbcc56d0cb064b14e41
ihorkrukovskyi777 <ihor.krukovskyi@gmail.com>
2025-10-24 12:38:27 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var swiper = new Swiper(".mySwiper", {
+        direction: "vertical",
+        slidesPerView: 1,
+        spaceBetween: 30,
+        mousewheel: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+    });
+});

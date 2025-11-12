a525228c4462408e760f3888fcb68eac0f133247
ihorkrukovskyi777 <ihor.krukovskyi@gmail.com>
2025-11-12 12:06:10 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var swiper = new Swiper(".mySwiper", {
-        direction: "vertical",
-        slidesPerView: 1,
-        spaceBetween: 30,
-        mousewheel: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    let swiperInstance;
+    const swiperContainer = document.querySelector(".mySwiper");
+
+    if (!swiperContainer) return; // Безпечна перевірка
+
+    function initSwiper() {
+        if (window.innerWidth > 1199 && !swiperInstance) {
+            swiperInstance = new Swiper(".mySwiper", {
+                direction: "vertical",
+                slidesPerView: 1,
+                spaceBetween: 30,
+                mousewheel: true,
+                pagination: {
+                    el: ".swiper-pagination",
+                    clickable: true,
+                },
+            });
+        } 
+        else if (window.innerWidth <= 1199 && swiperInstance) {
+            swiperInstance.destroy(true, true);
+            swiperInstance = undefined;
+        }
+    }
+
+    initSwiper();
+    window.addEventListener('resize', initSwiper);
 });
